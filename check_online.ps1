ed0cbe75cfc680f7c60dc456e32ff41c9761e7e8
Daniel Müller <39839325+muellerdan@users.noreply.github.com>
2021-09-06 07:34:46 +0200

    Initial Commit

---
+++ check_online.ps1
@@ -0,0 +1,10 @@
+﻿
+$computers = Get-ADComputer -Filter {OperatingSystem -Like "*server*"} -Properties operatingsystem | foreach {$_.DNSHostName}
+
+
+foreach ($computer in $computers) {
+    $connected = Test-Connection $computer -Count 1 -Quiet
+        if ($connected) {
+            Write-Host -ForegroundColor Green "$computer - Ist erreichbar"
+        }
+    }
