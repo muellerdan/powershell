d2698940ba854883aa1e4ed0196f90fe8f0a78a2
Daniel Müller <daniel.mueller@feelsfiber.net>
2022-04-13 09:46:05 +0200

    lancom section done

--- certificates.ps1
+++ certificates.ps1
@@ -1,4 +1,4 @@
-$Organization = "Branddirektion München"
+﻿$Organization = "Branddirektion München"
 $OrganizationUnit = "IT 25"
 $Country = "DE"
 $EMail = "bfm.it2-betrieb_alarmuebertragungsanlagen@muenchen.de"
@@ -17,21 +17,31 @@ if ($confirmation -eq "y") {
     $DeviceType = Read-Host "Um welche Art von Host handelt es sich? (Switch, Router, Server)"
     switch ($DeviceType) {
         switch {
-            <# Frage nach Lancom Switches fehlt noch. + .pem export sowie -----BEGIN RSA PRIVATE KEY----- replacement #>
             $confirmation_lancom = Read-Host "Handelt es sich um LanCom Switche? (y/n)"
             if ($confirmation_lancom -eq "y") {
+                $exportpwd = Read-Host "Bitte geben Sie das Kennwort für den Export ein" -AsSecureString
                 Get-Certificate -Template "Webserver-ExtendedValidation(2048)" -DnsName "$FQDN" -CertStoreLocation Cert:\LocalMachine\My -SubjectName "CN=$FQDN, C=$Country, L=$Location, O=$Organization, OU=$OrganizationUnit, S=$State, E=$Email"
                 $PFXCert = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -Match "$FQDN"} | Select-Object -ExpandProperty Thumbprint
-                Write-Host $PFXCert
-                Export-PfxCertificate 
+                Export-PfxCertificate -Cert Cert:\LocalMachine\My\$PFXCert -FilePath "C:\certs\$FQDN.pfx" -Password $exportpwd
+                openssl.exe pkcs12 -in "C:\certs\$FQDN.pfx" -out "C:\certs\$FQDN.pem"
+                $PEMContent = Get-Content -Path "C:\certs\$FQDN.pem" 
+                $GoodPEMContent = $PEMContent -replace '-----BEGIN ENCRYPTED PRIVATE KEY-----', '-----BEGIN RSA PRIVATE KEY-----'
+                $VeryGoodPEMContent = $GoodPEMContent -replace '-----END ENCRYPTED PRIVATE KEY-----', '-----END RSA PRIVATE KEY-----'
+                $VeryGoodPEMContent | Set-Content -Path "C:\certs\final\$FQDN.pem"
+
             }
-            Get-Certificate -Template "Webserver-ExtendedValidation(2048)" -DnsName "$FQDN" -CertStoreLocation Cert:\LocalMachine\My -SubjectName "CN=$FQDN, C=$Country, L=$Location, O=$Organization, OU=$OrganizationUnit, S=$State, E=$Email"
         }
         Router {
-
+          $exportpwd = Read-Host "Bitte geben Sie das Kennwort für den Export ein" -AsSecureString
+          Get-Certificate -Template "Webserver-ExtendedValidation" -DnsName "$FQDN" -CertStoreLocation Cert:\LocalMachine\My -SubjectName "CN=$FQDN, C=$Country, L=$Location, O=$Organisation, OU=$OrganizationUnit, S=$State, E=$Email"
+          $PFXCert = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -Match "$FQDN"} | Select-Object -ExpandProperty Thumbprint
+          Export-PfxCertificate -Cert Cert:\LocalMachine\My\$PFXCert -FilePath "C:\certs\$FQDN.pfx" -Password $exportpwd
         }
         Server {
-            Get-Certificate -Template "Webserver-ExtendedValidation" -DnsName "$FQDN" -CertStoreLocation Cert:\LocalMachine\My -SubjectName "CN=$FQDN, C=$Country, L=$Location, O=$Organisation, OU=$OrganizationUnit, S=$State, E=$Email"
+          $exportpwd = Read-Host "Bitte geben Sie das Kennwort für den Export ein" -AsSecureString
+          Get-Certificate -Template "Webserver-ExtendedValidation" -DnsName "$FQDN" -CertStoreLocation Cert:\LocalMachine\My -SubjectName "CN=$FQDN, C=$Country, L=$Location, O=$Organisation, OU=$OrganizationUnit, S=$State, E=$Email"
+          $PFXCert = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -Match "$FQDN"} | Select-Object -ExpandProperty Thumbprint
+          Export-PfxCertificate -Cert Cert:\LocalMachine\My\$PFXCert -FilePath "C:\certs\$FQDN.pfx" -Password $exportpwd
         }
      }
     }
