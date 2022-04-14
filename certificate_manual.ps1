9de170711b1f29e1ddfc253c687826cd545f3acb
Daniel Müller <daniel.mueller@feelsfiber.net>
2022-04-14 09:16:59 +0200

    removed encrypted private key upon convertion

--- certificate_manual.ps1
+++ certificate_manual.ps1
@@ -21,10 +21,11 @@ if ($confirmation -eq "y") {
                 Get-Certificate -Template "Webserver-ExtendedValidation(2048)" -DnsName "$FQDN" -CertStoreLocation Cert:\LocalMachine\My -SubjectName "CN=$FQDN, C=$Country, L=$Location, O=$Organization, OU=$OrganizationUnit, S=$State, E=$Email"
                 $PFXCert = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -Match "$FQDN"} | Select-Object -ExpandProperty Thumbprint
                 Export-PfxCertificate -Cert Cert:\LocalMachine\My\$PFXCert -FilePath "C:\certs\$FQDN.pfx" -Password $exportpwd
-                openssl.exe pkcs12 -in "C:\certs\$FQDN.pfx" -out "C:\certs\$FQDN.pem"
+                Get-ChildItem Cert:\LocalMachine\My\$PFXCert | Remove-Item
+                openssl.exe pkcs12 -in "C:\certs\$FQDN.pfx" -out "C:\certs\$FQDN.pem" -nodes
                 $PEMContent = Get-Content -Path "C:\certs\$FQDN.pem" 
-                $GoodPEMContent = $PEMContent -replace '-----BEGIN ENCRYPTED PRIVATE KEY-----', '-----BEGIN RSA PRIVATE KEY-----'
-                $VeryGoodPEMContent = $GoodPEMContent -replace '-----END ENCRYPTED PRIVATE KEY-----', '-----END RSA PRIVATE KEY-----'
+                $GoodPEMContent = $PEMContent -replace '-----BEGIN PRIVATE KEY-----', '-----BEGIN RSA PRIVATE KEY-----'
+                $VeryGoodPEMContent = $GoodPEMContent -replace '-----END PRIVATE KEY-----', '-----END RSA PRIVATE KEY-----'
                 $VeryGoodPEMContent | Set-Content -Path "C:\certs\final\$FQDN.pem"
             }
             else {
@@ -40,12 +41,14 @@ if ($confirmation -eq "y") {
           Get-Certificate -Template "Webserver-ExtendedValidation" -DnsName "$FQDN" -CertStoreLocation Cert:\LocalMachine\My -SubjectName "CN=$FQDN, C=$Country, L=$Location, O=$Organisation, OU=$OrganizationUnit, S=$State, E=$Email"
           $PFXCert = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -Match "$FQDN"} | Select-Object -ExpandProperty Thumbprint
           Export-PfxCertificate -Cert Cert:\LocalMachine\My\$PFXCert -FilePath "C:\certs\$FQDN.pfx" -Password $exportpwd
+          Get-ChildItem Cert:\LocalMachine\My\$PFXCert | Remove-Item
         }
         Server {
           $exportpwd = Read-Host "Bitte geben Sie das Kennwort für den Export ein" -AsSecureString
           Get-Certificate -Template "Webserver-ExtendedValidation" -DnsName "$FQDN" -CertStoreLocation Cert:\LocalMachine\My -SubjectName "CN=$FQDN, C=$Country, L=$Location, O=$Organisation, OU=$OrganizationUnit, S=$State, E=$Email"
           $PFXCert = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -Match "$FQDN"} | Select-Object -ExpandProperty Thumbprint
           Export-PfxCertificate -Cert Cert:\LocalMachine\My\$PFXCert -FilePath "C:\certs\$FQDN.pfx" -Password $exportpwd
+          Get-ChildItem Cert:\LocalMachine\My\$PFXCert | Remove-Item
         }
      }
     }
