55e65ad753985c3f370f4590ef422438fdd70fce
Daniel Müller <daniel.mueller@feelsfiber.net>
2022-04-14 09:15:31 +0200

    Cleaned code

--- certificate_automated.ps1
+++ certificate_automated.ps1
@@ -41,15 +41,3 @@ foreach ($device in $devices){
         Write-Host "Es wurde kein SSL Tag für $FQDN gefunden."
     }
 }
-
-
-<#
-    Get-Certificate -Template "Webserver-ExtendedValidation(2048)" -DnsName "$FQDN" -CertStoreLocation Cert:\LocalMachine\My -SubjectName "CN=$FQDN, C=$Country, L=$Location, O=$Organization, OU=$OrganizationUnit, S=$State, E=$Email"
-    $PFXCert = Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -Match "$FQDN"} | Select-Object -ExpandProperty Thumbprint
-    Export-PfxCertificate -Cert Cert:\LocalMachine\My\$PFXCert -FilePath "C:\certs\$FQDN.pfx" -Password $exportpwd
-    openssl.exe pkcs12 -in "C:\certs\$FQDN.pfx" -out "C:\certs\$FQDN.pem"
-    $PEMContent = Get-Content -Path "C:\certs\$FQDN.pem" 
-    $GoodPEMContent = $PEMContent -replace '-----BEGIN ENCRYPTED PRIVATE KEY-----', '-----BEGIN RSA PRIVATE KEY-----'
-    $VeryGoodPEMContent = $GoodPEMContent -replace '-----END ENCRYPTED PRIVATE KEY-----', '-----END RSA PRIVATE KEY-----'
-    $VeryGoodPEMContent | Set-Content -Path "C:\certs\final\$FQDN.pem"
-#>
